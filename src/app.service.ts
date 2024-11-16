0adf10ae0aa3d345739e56bd21159505d0ddd235
lcubas <langelcubas9@gmail.com>
2024-11-16 12:44:23 -0500

    Added and config soap server

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getData(args: { id: string }): Promise<{ data: string }> {
+    return new Promise((resolve) => {
+      resolve({ data: `Retrieved data for ID: ${args.id}` });
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
+import { createServer } from 'http';
+import * as soap from 'soap';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const service = app.get(AppService);
+  const myService = {
+    ExampleService: {
+      ExamplePort: {
+        // This is how to define an asynchronous function with a Promise.
+        GetData: async function (args: { id: string }) {
+          const result = await service.getData({ id: args.id });
+
+          return new Promise((resolve) => {
+            resolve(result);
+          });
+        },
+      },
+    },
+  };
+
+  const wsdlPath = join(__dirname, 'service.wsdl');
+  const wsdl = readFileSync(wsdlPath, 'utf8');
+
+  // http server example
+  const server = createServer(function (request, response) {
+    response.end('404: Not Found: ' + request.url);
+  });
+
+  server.listen(3000);
+  soap.listen(server, '/wsdl', myService, wsdl, function (error, res) {
+    console.log('server initialized', error, res);
+  });
 }
+
 bootstrap();
